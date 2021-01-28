84b8abc719b934bc8ba2b76ba8ac8b656432b92d
MuhammadAmmafF <67086053+MuhammadAmmafF@users.noreply.github.com>
2021-01-28 12:36:48 +0700

    Gass

---
+++ src/tetgithub/Tetgithub.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetgithub;
+
+/**
+ *
+ * @author M. `Ammar Fadhlan
+ */
+public class Tetgithub {
+    public static void main (String args []){
+        System.out.println("Hello World");
+           for(int i=0;i<10;i++){
+               System.out.println(i);
+           }
+    }
+}
